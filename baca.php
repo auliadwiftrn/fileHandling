0d9b1c5a756d51d7bf602f410d4b10f251748fca
FirdausiaIndah <37113085+FirdausiaIndah@users.noreply.github.com>
2018-04-22 03:08:49 +0700

    Menampilkan Isi Data Storage

--- baca.php
+++ baca.php
@@ -12,25 +12,68 @@ $baris = explode("[R]", $data);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="styles.css">
-    <title>Data</title>
-   
+    <title>Mencoba Agar Bisa</title>
+<style>
+table {
+    margin-top: 2rem;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid #c1c1c1;
+    border-collapse: collapse;
+    width: 50%;
+    font-size: 14px;
+    font-family: Verdana;
+}
+
+th, td {
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even){background-color: #f2f2f2}
+
+th {
+    background-color: #4CAF50;
+    color: white;
+}
+form {
+  padding: 1em;
+  background: #f9f9f9;
+  border: 1px solid #c1c1c1;
+  margin-top: 3rem;
+  max-width: 400px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 1em;
+}
+input.MyButton {
+margin-top: 3%;
+margin-left: 30%;
+width: 150px;
+padding: 5px;
+cursor: pointer;
+font-weight: bold;
+font-size: 150%;
+background: #3366cc;
+color: #fff;
+border: 1px solid #3366cc;
+border-radius: 2px;
+}
+input.MyButton:hover {
+color: gold;
+background: #c1c1c1;
+border: 1px solid #c1c1c1;
+}
+</style>
 </head>
-<body style="background-color:#2980b9;">
-<div class="container" >
-    <div class="row justify-content-center " style="margin-top:50px;">
-        <div class="col-xm-6 offset-xm-3" style="">
-            <div class="card" style="width: 55rem;">
-                    <table class="table table-bordered">
-                        <tr><th class="text-center"colspan =" 7"  > Biodata </th></tr>
+<body>
+<center><h1 style="margin-top: 5%; font-family:verdana;">BUKU TELEPON KU</h1></center>
+                    <table>
                         <tr>
-                            <td class="table-primary">Nama</td>
-                            <td class="table-primary">Email</td>
-                            <td class="table-primary">Phone</td>
-                            <td class="table-primary">Tanggal Lahir</td>
-                            <td class="table-primary">Hobi</td>
-                            <td class="table-primary" colspan=2>Action</td>
+                            <th >Nama</th>
+                            <th >Email</th>
+                            <th >Phone</th>
+                            <th colspan=2 style="text-align: center">Action</th>
                         </tr>
                     <?php
                     for($i =0; $i<count($baris)-1; $i++) {
@@ -39,28 +82,15 @@ $baris = explode("[R]", $data);
                     echo "  <td>" . $col[0] . "</td>";
                     echo "  <td>" . $col[1] . "</td>";
                     echo "  <td>" . $col[2] . "</td>";
-                    echo "  <td>" . $col[3] . "</td>";
-                    echo "  <td>" . $col[4] . "</td>";
                     echo '  <td> <a href="delete.php?row='.$i.'">DELETE</a> </td>';
                     echo '  <td> <a href="edit.php?row='.$i.'">EDIT</a> </td>';
                     echo "</tr>";
                         }
                         ?>
                     </table>
-                    <a href="form.html" style=" width:150px; margin-left:45.5rem;" class="btn btn-outline-dark btn-sm">Kembali Ke Form!</a>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
+                    <form>
+                    <label style="margin-left: 15%; font-size: 28px; font-family: verdana;" for="dataLagi" class="dataLagi">Mau input data lagi? </label><br>
+                    <input class="MyButton" type="button" value="Klik Disini" onclick="window.location.href='form.html'" />
+                    </form>
 </body>
 </html>
-
-
-
-
-    
-
-
